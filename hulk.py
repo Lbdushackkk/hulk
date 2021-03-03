2720adf24dda5547953d1b29123b233bb6392142
Lbdushackkk <79969780+Lbdushackkk@users.noreply.github.com>
2021-03-03 18:50:24 -0300

    lbattack.py

--- hulk.py
+++ hulk.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------------------------
-# HULK - HTTP Unbearable Load King
+# LB ATTACK - HTTP Unbearable Load King
 #
 # this tool is a dos tool that is meant to put heavy load on HTTP servers in order to bring them
 # to their knees by exhausting the resource pool, its is meant for research purposes only
